dccd6956b8ec8d4729bc33af86951ef5d100acb9
unknown <javibet13@gmail.com>
2021-03-29 16:20:05 -0300

    Agregué bootstrap, carpeta components y un navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,27 @@
 function App() {
     return <>
              <NavBar />
-             <h2>Las ofertas de la semana</h2>
-     </>;
+             <h1>Libreria Betancourt</h1>
+             <h2>Los mejores libros para su biblioteca</h2>
+            
+            <nav class="navbar navbar-expand-sm bg-light navbar-light">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Economía</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Historia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Religión</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Filosofía</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Sociología</a>
+                    </li>
+                </ul>
+            </nav>
+    </>;
 }
