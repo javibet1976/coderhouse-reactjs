d06a96d52ae29db1cdc971d0ee96304e70a52d31
unknown <javibet13@gmail.com>
2021-04-12 18:41:01 -0300

    Imagenes en desafio 6

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -7,24 +7,32 @@ const productos = [
     nombre: "Economía",
     caracteristica: "Libros de economía",
     precio: " 2.000 pesos",
+    imagen:
+      "https://www.marcialpons.es/media/img/portadas/2017/9/19/9788448612504.jpg",
   },
   {
     id: 2,
     nombre: "Filosofía",
     caracteristica: "Libros de filosofía",
     precio: " 3.000 pesos",
+    imagen:
+      "https://planetadelibrosar6.cdnstatics.com/usuaris/libros/fotos/266/m_libros/265495_portada_viaje-al-centro-de-la-filosofia_nemrod-carrasco_201803131003.jpg",
   },
   {
     id: 3,
     nombre: "Sociología",
     caracteristica: "Libros de sociología",
     precio: " 1.500 pesos",
+    imagen:
+      "https://imgserver1.fierro.com.ar/get/thumb/360/509/9789871086443.jpg",
   },
   {
     id: 4,
     nombre: "Religión",
     caracteristica: "Libros de religión",
     precio: " 2.500 pesos",
+    imagen:
+      "https://www.naoslibros.es/media/img/portadas/_visd_0000JPG01Z2A.jpg",
   },
 ];
 
@@ -51,7 +59,7 @@ class Item extends Component {
           {this.state.ciclos_de_vida.map((a) => {
             return (
               <li>
-                {a.nombre} - {a.caracteristica} - {a.precio}
+                {a.nombre} - {a.caracteristica} - {a.precio} - {a.imagen}
               </li>
             );
           })}
