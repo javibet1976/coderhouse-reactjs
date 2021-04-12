1e753023ae16c961ee70ac70dfa09313d6a89525
unknown <javibet13@gmail.com>
2021-04-12 18:57:59 -0300

    agregado de imagenes externas posta

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,33 +6,39 @@ const productos = [
     id: 1,
     nombre: "Economía",
     caracteristica: "Libros de economía",
-    precio: " 2.000 pesos",
-    imagen:
-      "https://www.marcialpons.es/media/img/portadas/2017/9/19/9788448612504.jpg",
+    precio: "2.000 pesos",
+    imagen: (
+      <img src="https://blog.bancosantander.es/wp-content/uploads/2018/08/compedio-basico-de-finanzas.jpg" />
+    ),
   },
+
   {
     id: 2,
     nombre: "Filosofía",
     caracteristica: "Libros de filosofía",
-    precio: " 3.000 pesos",
-    imagen:
-      "https://planetadelibrosar6.cdnstatics.com/usuaris/libros/fotos/266/m_libros/265495_portada_viaje-al-centro-de-la-filosofia_nemrod-carrasco_201803131003.jpg",
+    precio: "3.000 pesos",
+    imagen: (
+      <img src="https://planetadelibrosar6.cdnstatics.com/usuaris/libros/fotos/266/m_libros/265495_portada_viaje-al-centro-de-la-filosofia_nemrod-carrasco_201803131003.jpg" />
+    ),
   },
+
   {
     id: 3,
     nombre: "Sociología",
     caracteristica: "Libros de sociología",
-    precio: " 1.500 pesos",
-    imagen:
-      "https://imgserver1.fierro.com.ar/get/thumb/360/509/9789871086443.jpg",
+    precio: "1.500 pesos",
+    imagen: (
+      <img src="https://imgserver1.fierro.com.ar/get/thumb/360/509/9789871086443.jpg" />
+    ),
   },
   {
     id: 4,
     nombre: "Religión",
     caracteristica: "Libros de religión",
-    precio: " 2.500 pesos",
-    imagen:
-      "https://www.naoslibros.es/media/img/portadas/_visd_0000JPG01Z2A.jpg",
+    precio: "2.500 pesos",
+    imagen: (
+      <img src="https://libreriaclc.com/_CLCSpain/images/products/large/20258.jpg" />
+    ),
   },
 ];
 
